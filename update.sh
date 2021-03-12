8859200049a89efa8b96779e15314ca11d97e15a
Carsten Bittrich <carsten.bittrich@logmein.com>
2021-03-13 00:17:37 +0100

    Dokumentation und update.sh konfigurierbar

--- update.sh
+++ update.sh
@@ -1,17 +1,36 @@
 #/bin/bash
-CORONA=/Users/pavel/Corona
+
+# user specific settings
+# (defaults match pavel's settings, but can be overriden)
+if [ -z "$CORONA" ]
+then
+    CORONA=$HOME/Corona
+fi
+if [ -z "$GOOGLE_SDK_PATH" ]
+then
+    GOOGLE_SDK_PATH=/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+fi
+
+SHELLNAME=$(basename `echo $SHELL`)
+
+# prepare environment
 #cd $CORONA/2020-rki-archive
 #git pull
 cd $CORONA
 
-mkdir -p ard-data
 mkdir -p ard-data
 mkdir -p archive_ard
 mkdir -p archive_v2
 mkdir -p series
 mkdir -p series-enhanced
 
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+if [ -e $GOOGLE_SDK_PATH ]
+then
+    source $GOOGLE_SDK_PATH/path.$SHELLNAME.inc
+else
+    echo "Could not find Google Cloud sdk path at: $GOOGLE_SDK_PATH"
+fi
+
 gsutil rsync -d -r gs://brdata-public-data/rki-corona-archiv/ ard-data
 
 cd $CORONA/ard-data/2_parsed
